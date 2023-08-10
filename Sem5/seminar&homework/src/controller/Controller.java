9cb69d75635f47ab04da35b8a98e51b5a258fe77
Oleg  Shuliak <nformals@gmail.com>
2023-08-10 21:33:04 +0300

    Add homework tasks for seminar 5

--- Sem5/seminar&homework/src/controller/Controller.java
+++ Sem5/seminar&homework/src/controller/Controller.java
@@ -1,15 +1,18 @@
 package controller;
 
 import model.Student;
+import model.Teacher;
 import model.Type;
 import model.User;
 import service.DataService;
+import service.StudyGroupService;
 import view.StudentView;
 
 import java.util.List;
 
 public class Controller {
     DataService dataService = new DataService();
+    StudyGroupService studyGroupService = new StudyGroupService();
     public void createStudent(String firstName, String secondName, String lastName){
         dataService.create(firstName, secondName, lastName, Type.STUDENT);
     }
@@ -21,4 +24,9 @@ public class Controller {
             view.printConsole((Student) user);
         }
     }
+
+    public void FormNewGroup(Teacher teacher, List<Student> studentList){
+        studyGroupService.formStudyGroup(teacher, studentList);
+    }
+
 }
---
+++ Sem5/seminar&homework/src/service/StudyGroupService.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.Student;
+import model.Teacher;
+import model.User;
+
+import java.util.List;
+
+public class StudyGroupService {
+   private List<User> studyGroup;
+
+    public void formStudyGroup(Teacher teacher, List<Student> studentList){
+        studyGroup.add(teacher);
+        studyGroup.addAll(studentList);
+    }
+
+    public List<User> getStudyGroup(){
+        return studyGroup;
+    }
+}
